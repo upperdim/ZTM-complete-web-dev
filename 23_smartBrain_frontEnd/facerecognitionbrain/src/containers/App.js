fd8d3b09941bc1c5e546cf6ff3fe34c905d64e8e
upperdim <45048352+upperdim@users.noreply.github.com>
2022-07-28 21:55:44 +0300

    added input event handlers for the form

--- 23_smartBrain_frontEnd/facerecognitionbrain/src/containers/App.js
+++ 23_smartBrain_frontEnd/facerecognitionbrain/src/containers/App.js
@@ -7,93 +7,108 @@ import Rank from '../components/Rank/Rank'
 import Particles from "react-tsparticles"
 import { loadFull } from "tsparticles"
 
+const particlesInit = async (main) => {
+  // console.log(main);
+  await loadFull(main);
+};
+
+const particlesLoaded = (container) => {
+  // console.log(container);
+};
+
+// Might do: You can store this giant thing somewhere else and import it
+const particlesOptions = {
+  fpsLimit: 120,
+  interactivity: {
+  events: {
+    onClick: {
+    enable: true,
+    mode: "push",
+    },
+    onHover: {
+    enable: true,
+    mode: "repulse",
+    },
+    resize: true,
+  },
+  modes: {
+    push: {
+    quantity: 4,
+    },
+    repulse: {
+    distance: 200,
+    duration: 0.4,
+    },
+  },
+  },
+  particles: {
+  color: {
+    value: "#ffffff",
+  },
+  links: {
+    color: "#ffffff",
+    distance: 150,
+    enable: true,
+    opacity: 0.5,
+    width: 1,
+  },
+  collisions: {
+    enable: true,
+  },
+  move: {
+    direction: "none",
+    enable: true,
+    outModes: {
+    default: "bounce",
+    },
+    random: false,
+    speed: 2,
+    straight: false,
+  },
+  number: {
+    density: {
+    enable: true,
+    area: 800,
+    },
+    value: 80,
+  },
+  opacity: {
+    value: 0.5,
+  },
+  shape: {
+    type: "circle",
+  },
+  size: {
+    value: { min: 1, max: 5 },
+  },
+  },
+  detectRetina: true,
+}
+
 class App extends React.Component {
-  render() {
-    const particlesInit = async (main) => {
-      console.log(main);
-      await loadFull(main);
-    };
-  
-    const particlesLoaded = (container) => {
-      console.log(container);
-    };
-
-    // Might do: You can store this giant thing somewhere else and import it
-    const particlesOptions = {
-      fpsLimit: 120,
-      interactivity: {
-      events: {
-        onClick: {
-        enable: true,
-        mode: "push",
-        },
-        onHover: {
-        enable: true,
-        mode: "repulse",
-        },
-        resize: true,
-      },
-      modes: {
-        push: {
-        quantity: 4,
-        },
-        repulse: {
-        distance: 200,
-        duration: 0.4,
-        },
-      },
-      },
-      particles: {
-      color: {
-        value: "#ffffff",
-      },
-      links: {
-        color: "#ffffff",
-        distance: 150,
-        enable: true,
-        opacity: 0.5,
-        width: 1,
-      },
-      collisions: {
-        enable: true,
-      },
-      move: {
-        direction: "none",
-        enable: true,
-        outModes: {
-        default: "bounce",
-        },
-        random: false,
-        speed: 2,
-        straight: false,
-      },
-      number: {
-        density: {
-        enable: true,
-        area: 800,
-        },
-        value: 80,
-      },
-      opacity: {
-        value: 0.5,
-      },
-      shape: {
-        type: "circle",
-      },
-      size: {
-        value: { min: 1, max: 5 },
-      },
-      },
-      detectRetina: true,
+  constructor() {
+    super()
+    this.state = {
+      input: '',
     }
+  }
 
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onSubmit = () => {
+    console.log('click')
+  }
+
+  render() {
     return (
       <div className='App'>
         <Particles className='Particles' init={particlesInit} loaded={particlesLoaded} options={particlesOptions}	/>
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onSubmit} />
         {/*<FaceRecognition /> */}
       </div>
     )
