f3f9c3c4bad0020f35d43b33d41959f34220fcb7
upperdim <45048352+upperdim@users.noreply.github.com>
2022-07-29 22:10:02 +0300

    completed face recognition

--- 23_smartBrain_frontEnd/facerecognitionbrain/src/containers/App.js
+++ 23_smartBrain_frontEnd/facerecognitionbrain/src/containers/App.js
@@ -4,8 +4,16 @@ import Navigation from '../components/Navigation/Navigation'
 import Logo from '../components/Logo/Logo'
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm'
 import Rank from '../components/Rank/Rank'
+import FaceRecognition from '../components/FaceRecognition/FaceRecognition'
+import Signin from '../components/Signin/Signin'
 import Particles from "react-tsparticles"
 import { loadFull } from "tsparticles"
+import Clarifai from 'clarifai'
+import clarifaiApiKey from './clarifaiApiKey'
+
+const app = new Clarifai.App({
+  apiKey: clarifaiApiKey
+})
 
 const particlesInit = async (main) => {
   // console.log(main);
@@ -89,16 +97,40 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      input: '',
+      input: '',    // Current input the URL input box
+      imageUrl: '', // URL of the image to be processed by the AI API
+      box: {},      // Face boundary box in the image
+      route: ''     // Keeps track of where we are in the page
     }
   }
 
-  onInputChange = (event) => {
-    console.log(event.target.value)
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputImage')
+    const width = Number(image.width)
+    const height = Number(image.height)
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
   }
 
-  onSubmit = () => {
-    console.log('click')
+  displayFaceBox = (box) => {
+    this.setState({box: box})
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    // Using `this.state.imageUrl` would give an error because of the way setState() works. Hard to debug...
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err))
   }
 
   render() {
@@ -106,10 +138,11 @@ class App extends React.Component {
       <div className='App'>
         <Particles className='Particles' init={particlesInit} loaded={particlesLoaded} options={particlesOptions}	/>
         <Navigation />
+        <Signin />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onSubmit} />
-        {/*<FaceRecognition /> */}
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     )
   }
