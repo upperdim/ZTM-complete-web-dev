b60ba7922e98170346a6327773670a0fb8cd4bdc
upperdim <45048352+upperdim@users.noreply.github.com>
2022-08-07 03:06:44 +0300

    added getting user profiles

--- 27_smartBrain_backEnd_database/backend/server.js
+++ 27_smartBrain_backEnd_database/backend/server.js
@@ -104,14 +104,17 @@ app.post('/register', (req, res) => {
 app.get('/profile/:id', (req, res) => {
 	// Destructure profile id from the request param
 	const { id } = req.params
-	// Search users for that id
-	database.users.forEach(user => {
-		if (user.id === id) {
-			return res.json(user)
-		}
-	})
-	// That id was not found
-	res.status(404).json('no such user')
+	db.select('*').from('users').where({id: id})
+		.then(user => {
+			// That id was not found
+			if (user.length === 0) {
+				// We can't use `.catch()` to check if no users was found because
+				// instead of giving an error, database will simply return an empty array
+				res.status(404).json('no such user')
+			}
+			res.json(user[0])
+		})
+		.catch(err => res.status(400).json('error getting user'))
 })
 
 // PUT is for updating
