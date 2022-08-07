5ab143fd3705dfb6981f967ff068eebcdab28aae
upperdim <45048352+upperdim@users.noreply.github.com>
2022-08-07 17:38:09 +0300

    move register function to a controller file

--- 28_production_deployment/backend/server.js
+++ 28_production_deployment/backend/server.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 const dbPassword = require('./dbPassword.js')
 const knex = require('knex')
 
+const register = require('./controllers/register.js')
+
 const db = knex({
 	client: 'pg',                        // pg stands for postgresSQL
 	connection: {
@@ -50,45 +52,8 @@ app.post('/signin', (req, res) => {
 		.catch(err => res.status(400).json('wrong credentials'))
 })
 
-app.post('/register', (req, res) => {
-	// Create a user with incoming info
-	const { email, name, password } = req.body
-	const hash = bcrypt.hashSync(password)
-	// Chain 2 db operations together so if one fails, other one should too.
-	// This is for avoiding inconsistencies, read 27_notes.txt
-	db.transaction(trx => {
-		trx.insert({
-			hash: hash,
-			email: email
-		})
-		.into('login')
-		.returning('email')
-		.then(loginEmail => {
-			// Our `users` table in the database has columns:
-			//     id     : auto increments, we don't need to provide it
-			//     name   : which we provide
-			//     email  : which we provide
-			//     entries: that defaults to 0, so we don't need to provide it
-			//     date   : which we provide as the current date
-			trx('users')
-			.returning('*') // after inserting, return/response with all rows that have been inserted (will insert only 1 anyway)
-			.insert({
-				email: loginEmail[0].email,
-				name: name,
-				joined: new Date()
-			})
-			.then(result => {
-				// result was the new user that has been inserted
-				res.json(result[0]) // Return the created user
-				// result is all the newly inserted values. we insert only 1 so it's always an array of 1. 
-				// we still return only the first element instead of sending the array form
-			})
-			.catch(err => res.status(400).json('unable to register'))
-		})
-		.then(trx.commit)
-		.catch(trx.rollback)
-	})
-})
+// Below is a dependency injection. We're passing dependencies of the function into it.
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 app.get('/profile/:id', (req, res) => {
 	// Destructure profile id from the request param
